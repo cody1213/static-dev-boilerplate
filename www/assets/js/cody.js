ddd09a7f465df1d93bfdcb79d8fb22f5076f2707
Cody Brumfield <cody_brumfield@hotmail.com>
2015-06-22 21:44:10 -0500

    added helper libraries

---
+++ www/assets/js/cody.js
@@ -0,0 +1,171 @@
+//These are some functions I use all the time.  
+
+var Cody = {
+  id2String = function(obj) {
+    return obj && obj._id?obj._id.toString():false;
+  },
+  getOrdinal:  function(n) {
+     var s=["th","st","nd","rd"],
+         v=n%100;
+     return n+(s[(v-20)%10]||s[v]||s[0]);
+  },
+  queryStringToHash: function(query) {
+    if (query == '') return null;
+    var hash = {};
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      var k = decodeURIComponent(pair[0]);
+      var isArray = false;
+      if (k.substr(k.length-2) == '[]') {
+        isArray = true;
+        k = k.substring(0, k.length - 2);
+      }
+      var v = decodeURIComponent(pair[1]);
+      // If it is the first entry with this name
+      if (typeof hash[k] === "undefined") {
+        if (isArray)  // not end with []. cannot use negative index as IE doesn't understand it
+          hash[k] = [v];
+        else
+          hash[k] = v;
+      // If subsequent entry with this name and not array
+      } else if (typeof hash[k] === "string") {
+        hash[k] = v;  // replace it
+      // If subsequent entry with this name and is array
+      } else {
+        hash[k].push(v);
+      }
+    }
+    _.each(hash, function(index, key) {})
+    return hash;
+  },
+  getUrlVars: function() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+     vars[key] = value;
+    });
+    return vars;
+  }
+}
+
+//jQuery additions
+
+
+//Deserializes query strings
+;(function ($) {
+  $.deserialize = function (query, options) {
+    if (query == '') return null;
+    var hash = {};
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      var k = decodeURIComponent(pair[0]);
+      var isArray = false;
+      if (k.substr(k.length-2) == '[]') {
+        isArray = true;
+        k = k.substring(0, k.length - 2);
+      }
+      var v = decodeURIComponent(pair[1]);
+      // If it is the first entry with this name
+      if (typeof hash[k] === "undefined") {
+        if (isArray)  // not end with []. cannot use negative index as IE doesn't understand it
+          hash[k] = [v];
+        else
+          hash[k] = v;
+      // If subsequent entry with this name and not array
+      } else if (typeof hash[k] === "string") {
+        hash[k] = v;  // replace it
+      // If subsequent entry with this name and is array
+      } else {
+        hash[k].push(v);
+      }
+    }
+    return hash;
+  };
+  $.fn.deserialize = function (options) {
+    return $.deserialize($(this).serialize(), options);
+  };
+})(jQuery);
+
+
+//Add $.put and $.delete functions
+;(function( $ ){
+  jQuery.each( [ "put", "delete" ], function( i, method ) {
+    jQuery[ method ] = function( url, data, callback, type ) {
+      if ( jQuery.isFunction( data ) ) {
+        type = type || callback;
+        callback = data;
+        data = undefined;
+      }
+   
+      return jQuery.ajax({
+        url: url,
+        type: method,
+        dataType: type,
+        data: data,
+        success: callback
+      });
+    };
+  });
+})( jQuery );
+
+//Prototypes
+
+//validation function
+String.prototype.validateAs = function(type) {
+  var re;
+  if (!type) {
+    return false;
+  }
+  if (type == "email") {
+    //from jquery validate plugin
+    re = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+  } else if (type == "mongoid" || type == "objectid") {
+    //24 digit hexadecimal
+    re = /^[0-9a-fA-F]{24}$/;
+  } else if (type == "required") {
+    re = /\S/;
+  } else if (type == "integer") {
+    re = /^\s*(\+|-)?\d+\s*$/;
+  } else if (type == "url" || type == "FQDN") {
+    //from jquery validate plugin
+    re = /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
+  } else if (type == "link") {
+    re = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+  } else if (type == "date") {
+    return !/Invalid|NaN/.test(new Date(this));
+  } else if (type == "domain") {
+    re = /^[a-z0-9]+[a-z0-9-\.]*[a-z0-9]+\.[a-z\.]{2,5}$/
+  } else if (type == "number" || type == "numeric") {
+    re = /^-?\d+\.?\d*$/;
+  } else if (type == "alphanumeric") {
+    re = /^[a-z0-9]+$/i;
+  } else if (type == "binary") {
+    re = /^[01]+$/;
+  } else if (type == "hexadecimal") {
+    re = /^[a-f0-9]+$/i;
+  } else if (type == "creditcard") {
+    re = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
+  } else {
+    return false;
+  }
+  return re.test(this);
+};
+
+String.prototype.toTitleCase = function () {
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
+String.prototype.trim = String.prototype.trim || function() {
+  return this.replace(/^\s+|\s+$/,"");
+}
+
+String.prototype.nl2br = function() {
+  return this.replace(/(\r\n|\r|\n)/g, "<br />");
+}
+String.prototype.br2nl = function() {
+  return this.replace(/<br \/>|<br\/>/g, "\n");
+}
+String.prototype.stripTags = function() {
+  return this.replace(/<\S[^>]*>/g, "");
+}
