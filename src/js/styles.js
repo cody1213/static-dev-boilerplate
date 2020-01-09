938525b0130198089686964fcdb6d4fc48f0a3a4
Cody Brumfield <cody_brumfield@hotmail.com>
2020-01-09 10:57:16 -0600

    bootswatch (optional)

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,3 +1,3 @@
 // Get Bootstrap styles with cssify
 require('../../node_modules/bootstrap/dist/css/bootstrap.css')
-require('../../node_modules/bootswatch/dist/yeti/bootstrap.min.css')
+//require('../../node_modules/bootswatch/dist/yeti/bootstrap.min.css')
